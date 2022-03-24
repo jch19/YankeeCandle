848a86382bf5efb5dea75dfdd9a13fd5238f4425
kkirkley2019 <60412966+kkirkley2019@users.noreply.github.com>
2022-03-24 17:13:19 -0400

    Add files via upload

---
+++ yankeecandle/src/yankeecandle/ViewInventoryController.java
@@ -0,0 +1,43 @@
+
+
+package yankeecandle;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class ViewInventoryController  {
+   
+    @FXML
+    private AnchorPane pane;
+    
+    
+    @FXML
+    void signout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        stage.setTitle("YankeeCandle");
+        stage.setScene(scene);
+        stage.show();
+
+       ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+        //will close program
+    }
+    
+
+}
